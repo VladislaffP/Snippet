174a241947b5648b1b36bff31f626c067758bd0a
VVP <vladislav_p@mail.ru>
2023-02-22 19:28:44 +0300

    ошибка

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from MainApp.models import Snippet
-from MainApp.forms import SnippetForm
+from MainApp.models import SnippetForm
+from MainApp.forms import SnippetForm, UserRegistrationForm
 from django.contrib import auth
 
-
+from Snippets.MainApp.forms import UserRegistrationForm
 
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
-
+@login_required()
 def add_snippet_page(request):
     if request.method =='GET':
         form = SnippetForm()
@@ -24,7 +25,7 @@ def add_snippet_page(request):
         #code = request.POST["code"]
         #snippet = Snippet(name=name, lang=lang, code=code)
         #snippet.save()
-        form =SnippetForm(request.POST)
+        form = SnippetForm(request.POST)
         if form.is_valid():
             snippet = form.save(commit=False)
             snippet.user = request.user
@@ -38,7 +39,7 @@ def snippets_page(request):
     #    sn = Snippet.objects.filter(user__username=None)
     #else:
     #    sn = Snippet.objects.filter(user__username=username)
-    sn = Snippet.objects.filter(is_public=True)
+    sn = SnippetForm.objects.filter(is_public=True)
     #cnt = Snippet.objects.count()
     cnt = sn.count()
     context = {
@@ -48,7 +49,7 @@ def snippets_page(request):
     return render(request, 'pages/view_snippets.html', context)
 
 def snippet_detail(request, snippet_id):
-    snippet = Snippet.objects.get(id=snippet_id)
+    snippet = SnippetForm.objects.get(id=snippet_id)
     context = {
         'pagename': 'Просмотр сниппетов',
         'snippet': snippet
@@ -57,7 +58,7 @@ def snippet_detail(request, snippet_id):
 
 
 def snippet_delete(request, snippet_id):
-    snippet = Snippet.objects.get(id=snippet_id)
+    snippet = SnippetForm.objects.get(id=snippet_id)
     snippet.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
 
@@ -80,14 +81,27 @@ def logout(request):
     return redirect('home')
     #return redirect(request.META.get('HTTP_REFERER', '/'))
 
+@login_required()
 def snippets_my(request):
     username = request.user
-    sn = Snippet.objects.filter(user__username=username)
+    sn = SnippetForm.objects.filter(user__username=username)
     cnt = sn.count()
     context = {
         "sn": sn,
         "cnt": cnt
     }
     return render(request, 'pages/view_snippets.html', context)
+
+def create_user(request):
+    context = {'pagename': 'Регистрация пользователя'}
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context["form"] = form
+        return render(request, 'pages/registration.html', context)
+    elif request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
 
